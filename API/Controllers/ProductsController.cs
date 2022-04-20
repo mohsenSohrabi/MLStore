5c9e6ba7a4d9b064d5b85333ad029ff6cc60cdb3
Mohsen Sohrabi <mohsen.sohrabi.cs@gmail.com>
2022-04-20 09:25:18 +0430

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController:ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product =  await _context.Products.FindAsync(id);
+            if(product == null){
+                return BadRequest("Product not found");
+            }
+            else{
+                return Ok(product);
+            }
+        }
+
+
+
+    }
+}
